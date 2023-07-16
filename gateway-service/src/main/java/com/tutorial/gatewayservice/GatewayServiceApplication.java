27d9fec2c8e4a26e753b9f3cab6c9c30be35b506
cristowo <cristobal.marchant@usach.cl>
2023-07-15 21:05:43 -0400

    em🤓🤓🤬🤬

--- gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package tingeso3.scriptservice;
+package com.tutorial.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ScriptServiceApplication {
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScriptServiceApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
